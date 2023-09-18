eeb5ec63d6cacc51d2b23141a9297fe3a38b7337
Mileny-Nogueira <milenynogueira.oficial.br@gmail.com>
2023-09-17 21:22:21 -0300

    delimitando as constantes para acessar o banco

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+    // Definindo as constantes de protocolo de acesso, link do servidor, nome do banco, login local e senha
+     const dbDrive = 'mysql';
+     const dbHost = 'localhost';
+     const dbName = 'db_vidda';
+     const dbUser = 'root';
+     const dbPass = '';
+?>
---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+    include 'PacientesService.php';
+   
+    header("Content-Type: application/json; charset=UTF-8");     
+   
+   
+    if ($_GET['url']){
+       
+        $url = explode('/' , $_GET['url']);
+         
+        if ($url[0] === 'api' ){
+            array_shift($url);
+            
+            $service = ucfirst($url[0]).'Service' ;  
+
+            array_shift($url); 
+            
+            $method = strtolower( $_SERVER['REQUEST_METHOD']); 
+
+            try {
+               
+                $response =  call_user_func_array(array( new  $service , $method), $url) ;
+                
+                http_response_code(200) ; // ok
+             
+                echo json_encode( array('status' => 'sucess' , 'data' => $response));
+                
+            } catch (Exception $e) {
+                http_response_code(404) ; // erro
+                
+                echo json_encode( array('status' => 'error' , 'data' => $e->getMessage()));
+            }  
+
+        } 
+    }
